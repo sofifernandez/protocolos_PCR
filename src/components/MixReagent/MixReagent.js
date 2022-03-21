5bd712c072a59c630a007a9380944353a07f4c85
Sofia Fernandez <sofiafernandez03@gmail.com>
2022-03-21 16:26:08 -0300

    text-wrap in cards

--- src/components/MixReagent/MixReagent.js
+++ src/components/MixReagent/MixReagent.js
@@ -3,11 +3,11 @@ import './MixReagent.scss'
 export const MixReagent = ({name, volume}) => {
     return (
 
-        <div className=' col-12 row justify-content-evenly mb-3'>
-            <div className='col-9 col-sm-7 col-lg-7  propertyTitleMix row'>
+        <div className=' col-12 row justify-content-evenly mb-3 px-0'>
+            <div className='col-10 col-sm-7 col-lg-7  propertyTitleMix row'>
                 <div className='col-3 titleCircle my-auto'>
                 </div>
-                <div className='col-9 fw-bold fs-2 my-auto'>
+                <div className='col-9 fw-bold fs-3 my-auto text-break'>
                     {name}
                 </div>
             </div>
